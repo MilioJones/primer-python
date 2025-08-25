972639b2d23121998992cae27a4d3d6ab9931561
MilioJones <emilioruba@gmail.com>
2025-08-25 16:52:38 +0200

    Add files via upload

---
+++ anos2100.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cómo te llamas? ")
+edad = int(input("¿Cuántos años tienes? "))
+
+año_2100 = 2100
+edad_2100 = edad + (año_2100 - 2025)
+
+print(f"Hola {nombre}, en el año {año_2100} tendrás {edad_2100} años.")
